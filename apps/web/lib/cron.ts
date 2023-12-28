dd92800fa7d3126af05322da7f4e5702f2b4fa18
VishnuVVR-369 <vishnuvardhanganji@gmail.com>
2023-12-28 20:22:12 +0530

    Modified minTime for cron to 10s

--- apps/web/lib/cron.ts
+++ apps/web/lib/cron.ts
@@ -4,7 +4,7 @@ import Bottleneck from "bottleneck";
 // we're using Bottleneck to avoid running into Resend's rate limit of 10 req/s
 export const limiter = new Bottleneck({
   maxConcurrent: 1, // maximum concurrent requests
-  minTime: 100, // minimum time between requests in ms
+  minTime: 10, // minimum time between requests in ms
 });
 
 // we're using Upstash's Receiver to verify the request signature
